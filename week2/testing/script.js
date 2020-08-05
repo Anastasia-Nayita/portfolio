72d03d26fdb306ec22751fabe8c7a540b07467f3
Anastasia Kekelidou <kekelidou@gmail.com>
2020-08-05 16:35:02 +0200

    margins 🙃

--- week2/testing/script.js
+++ week2/testing/script.js
@@ -1,24 +1,26 @@
 function changeColor(event) {
-    var box = document.getElementById("box");
+    var cube = document.getElementById("cube");
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    box = event.target;
+    cube = event.target;
     var newColor = "#" + randomColor;
-    box.style.backgroundColor = newColor;
+    cube.style.backgroundColor = newColor;
     console.log(newColor);
 }
 document.addEventListener("click", changeColor);
 
 function searchForSame() {
-    var box = document.getElementById("box");
-    for (var i = 0; i < box.length; i++) {
+    var cube = document.getElementById("cube");
+    for (var i = 0; i < cube.length; i++) {
         if (
-            box[i].style.backgroundColor == box[i + 1].style.backgroundColor &&
-            box[i].style.backgroundColor == box[i + 2].style.backgroundColor
+            cube[i].style.backgroundColor ==
+                cube[i + 1].style.backgroundColor &&
+            cube[i].style.backgroundColor == cube[i + 2].style.backgroundColor
         ) {
             console.log("👑wow");
         } else if (
-            box[i].style.backgroundColor == box[i + 3].style.backgroundColor &&
-            box[i].style.backgroundColor == box[i + 6].style.backgroundColor
+            cube[i].style.backgroundColor ==
+                cube[i + 3].style.backgroundColor &&
+            cube[i].style.backgroundColor == cube[i + 6].style.backgroundColor
         ) {
             console.log("🐥");
         }
