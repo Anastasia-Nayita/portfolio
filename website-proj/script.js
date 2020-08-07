ca0663a0c85ded1d65caeed627a7004e91c5d708
Anastasia Kekelidou <kekelidou@gmail.com>
2020-08-07 16:02:39 +0200

    in process

--- website-proj/script.js
+++ website-proj/script.js
@@ -1,21 +1,24 @@
 var burger = document.getElementById("menu");
 var overlay = document.getElementsByClassName("overlay");
 var nav = document.getElementsByClassName("side-nav");
+console.log(burger);
+console.log(overlay);
+console.log(nav);
 //var disappear = document.getElementsByClassName("disappear");
 
-var xxx = document.getElementById("x");
+var close = document.getElementById("close");
 // var sidelink = document.getElementsByClassName("side-nav-link");
 
 burger.addEventListener("click", function () {
-    overlay.classList.remove("disappear");
-    nav.classList.remove("disappear");
+    overlay[0].classList.remove("disappear");
+    nav[0].classList.remove("disappear");
 
     // sidelink[].classList.add("appear");
     // overlay[0].style.visibilyty = "visible";
     // nav[0].style.visibilyty = "visible";
 });
 
-// xxx[0].addEventListener("click", function () {
-//     overlay[0].classList.remove("appear");
-//     nav.classList.remove("appear");
-// });
+close.addEventListener("click", function () {
+    overlay[0].classList.add("disappear");
+    nav[0].classList.add("disappear");
+});
