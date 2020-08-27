9bf90453f50d79894533025526165f258a1346a3
Anastasia Kekelidou <kekelidou@gmail.com>
2020-08-27 12:17:07 +0200

    added auth

--- week5/expressPractise/index.js
+++ week5/expressPractise/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 const cookieParser = require("cookie-parser");
+const basicAuth = require("basic-auth");
 
 app.use(
     express.urlencoded({
@@ -10,30 +11,43 @@ app.use(
 app.use(cookieParser()); // has to go first (before static)
 
 app.use((req, res, next) => {
-    ////// as general in the top-middle.
-    // console.log("running middleware");
-    //// if (req.path != "/cookie") {
-    //     res.redirect("/cookie");
-    // }
-    if (!req.cookies.authenticated && req.path != "/cookie") {
-        res.cookie("path", req.path);
+    if (!req.cookies.authenticated && req.url !== "/cookie") {
+        res.cookie("path", req.url);
         res.redirect("/cookie");
     } else {
         next();
     }
 });
 
+const auth = function (req, res, next) {
+    const creds = basicAuth(req);
+    if ((req.path = __dirname + "/projects/connect4")) {
+        if (!creds || creds.name != "123" || creds.pass != "123") {
+            res.setHeader(
+                "WWW-Authenticate",
+                'Basic realm="Enter your credentials to see this stuff."'
+            );
+            res.sendStatus(401);
+        } else {
+            next();
+        }
+    }
+};
+app.use("/connect4", auth);
+
 app.use(express.static("./projects"));
 
 app.get("/", (req, res) => {
-    res.send("<h1>HOMEPAGE - Hello!</h1>");
+    res.redirect("/panes");
 });
+
 app.get("/cookie", (req, res) => {
     console.log("req.cookies : ", req.cookies);
-    res.send(`<h1>🍪 🍪 🍪 🍪</h1>
+    console.log("res.cookies : ", res.cookies);
+    // console.log("req.cookie path : ", req.cookie("path"));
+    res.send(`<h1> 🍪 🍪 🍪 🍪 🍪 🍪 </h1>
     <h2>To use this website you must accept cookies</h2>
-        <form method='POST' style="display: flex; flex-direction: column; justify-content:     space-between; width: 40%; height: 50%;">
-            
+        <form method='POST' style="display: flex; flex-direction: column; justify-content: space-between; width: 40%; height: 50%;">
             <div>
                 <input type="checkbox" name="accept"><span>Accept</span>
             </div>
@@ -42,10 +56,10 @@ app.get("/cookie", (req, res) => {
 });
 
 app.post("/cookie", (req, res) => {
-    res.cookie("authenticated", true);
     const { accept } = req.body;
     if (accept) {
-        res.redirect("/projects");
+        res.cookie("authenticated", true);
+        res.redirect(req.cookies.path); //read cookie and take to original loc they tryid to go
     } else {
         res.send(`
             <h1>Think again about accepting cookies..</h1>
@@ -58,15 +72,13 @@ app.get("/projects", (req, res) => {
     if (req.cookies.authenticated) {
         res.send(`
             <h1>⚰️ ✋🏻 TOP SECRET POJECTS ⛔️ 🤐</h1>
-            <h2>  </h2>
         `);
     } else {
         res.redirect("/cookie");
     }
 });
 
-// app.get(__dirname, (req, res) => {
-//     res.sendFile(req.path + "/index.html");
-// });
+// const pathToProj = __dirname + "/projects";
+// console.log("pathToProj = ", pathToProj);
 
 app.listen(8080, () => console.log("server is listenning..."));
